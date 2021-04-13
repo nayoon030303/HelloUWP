a8fd4b7273514a8eaa8508aedab65fa8523a747b
Nayoon Kim <nayoon030303@gmail.com>
2021-04-13 12:23:21 +0900

    first commit

---
+++ HelloUWP/Views/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using HelloUWP.ViewModels;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace HelloUWP.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel ViewModel => DataContext as MainViewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var myDialog = new MessageDialog("안녕하세용");
+            myDialog.Commands.Add(new UICommand("OK"));
+            myDialog.Commands.Add(new UICommand("CANCEL"));
+            myDialog.Commands.Add(new UICommand("BYE"));
+            await myDialog.ShowAsync();
+        }
+    }
+}
